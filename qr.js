8a9280e5ecfa003c818355f932f983fb62068db6
Ariyankingariy <98979444+Ariyankingariy@users.noreply.github.com>
2025-06-27 01:20:21 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -17,7 +17,7 @@ const { Boom } = require("@hapi/boom");
 const PORT = process.env.PORT ||  5000
 const MESSAGE = process.env.MESSAGE ||  `
 ┌───⭓『
-❒ *WASI-MD*
+❒ *SUVO-MD*
 ❒ _NOW DEPLOY IT_
 └────────────⭓
 ┌───⭓
